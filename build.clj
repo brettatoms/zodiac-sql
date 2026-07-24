43234703722fdbbf19774fcf2ea4c2db38671356
Brett Adams <brett@bletia-9.local>
2026-07-23 21:04:43 -0400

    refactor ci

--- build.clj
+++ build.clj
@@ -53,27 +53,13 @@
            :pom-data  (pom-template version))))
 
 (defn jar [opts]
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis @basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
-  (b/jar (jar-opts opts)))
-
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (test opts)
-  (b/delete {:path "target"})
   (let [opts (jar-opts opts)]
-    (println "\nWriting pom.xml...")
+    ;; Pass the full opts (incl. :pom-data) so the POM carries the license,
+    ;; SCM, etc. Clojars rejects a POM without a license.
     (b/write-pom opts)
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"]
+    (b/copy-dir {:src-dirs ["src" "resources"]
                  :target-dir class-dir})
-    (println "\nBuilding" (:jar-file opts) "...")
-    (b/jar opts))
-  opts)
+    (b/jar opts)))
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
   (let [{:keys [jar-file] :as opts} (jar-opts opts)]
